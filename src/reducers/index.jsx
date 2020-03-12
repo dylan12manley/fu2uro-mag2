cb5834b0451af0d9d0966da3a157bb6545cef59a
Dylan Manley <dylan.manley@gmail.com>
2020-03-11 23:17:40 -0700

    add add article from constants

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,7 +1,18 @@
 import { combineReducers } from "redux";
+import { ADD_ARTICLE } from "../constants/action-types";
 
-const rootReducer = combineReducers({
 
-});
+const initialState = {
+  articles: []
+};
+
+function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE) {
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
+  }
+  return state;
+}
 
 export default rootReducer;
